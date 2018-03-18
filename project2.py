6f2734533ddea7c7eef84728feb236e25ba878ab
stephen-bapple <stephen.bapple@gmail.com>
2018-03-18 16:13:37 -0600

    Removed more uneeded files.

--- project2.py
+++ project2.py
@@ -4,11 +4,14 @@ Project #2 for Numerical Analysis II
 This project revolves around applying various optimization techniques to the
 following system: TBD
 
-Stephen Bapple
+Optimization techniques used are found in optimizers.py
+
+Author: Stephen Bapple
 
 '''
 
 import numpy as np
+import optimizers
 
 if __name__ == '__main__':
     pass
